0067901ae03de295919af28b770ab9c31ab519ad
Sajib <sajibcse14@gmail.com>
2025-04-14 10:17:20 +0600

    Initialize Project : initial commit

---
+++ src/test/java/com/bigbuggers/spring_mvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigbuggers.spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
