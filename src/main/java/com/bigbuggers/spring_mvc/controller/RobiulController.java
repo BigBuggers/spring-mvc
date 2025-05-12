a323c0b7286699ae063b908991e72c288179bfcc
shawonNirob <shawonnirob16@gmail.com>
2025-05-13 01:20:40 +0600

    listController, listService, listDTO, and GlobalExceptionHandler added

--- src/main/java/com/bigbuggers/spring_mvc/controller/RobiulController.java
+++ src/main/java/com/bigbuggers/spring_mvc/controller/RobiulController.java
@@ -19,5 +19,4 @@ public class RobiulController {
         return "greeting";
     }
 
-
 }
---
+++ src/main/java/com/bigbuggers/spring_mvc/dto/ListDTO.java
@@ -0,0 +1,49 @@
+package com.bigbuggers.spring_mvc.dto;
+
+public class ListDTO {
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
